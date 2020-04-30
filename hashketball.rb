5498cf99ada5e65e8c634c274308d114a73c6455
Seoyoung Kim <mintholic1@gmail.com>
2020-04-30 22:06:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,8 +190,8 @@ def big_shoe_rebounds
   rebound_number = 0
   game_hash.each do |team, info|
     info[:players].each do |player_hash|
-      if player_hash[:size] > big_foot
-        big_foot = player_hash[:size]
+      if player_hash[:shoe] > big_foot
+        big_foot = player_hash[:shoe]
         rebound_number = player_hash[:rebounds]
       end
     end
